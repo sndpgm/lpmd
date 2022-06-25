1941c57a22c91f4f16501f12c8688deaef9dea2d
sndpgm <108000555+sndpgm@users.noreply.github.com>
2022-06-25 17:09:31 +0900

    CLN/ENH: cleanup and enhance scraper script (#8)

--- lpmd/tests/core/test_scrape.py
+++ lpmd/tests/core/test_scrape.py
@@ -1,48 +1,182 @@
 """pytest for lpmd.core.untable.scrape."""
+
+import os
 import shutil
 
+import numpy as np
 import pandas as pd
 import pytest
+import yaml
 
-import lpmd.core.unstable.scrape as scp
+from lpmd.core.scrape import BaseScraper, ScraperShipment
 
-test_url = {
-    "effective": "https://www.e-stat.go.jp/stat-search/file-download?statInfId=000032117924&fileKind=0",
-    "ineffective": "http://xxxxxx/dummy/test/xxxxxx.jp",
-}
+# -------------------------
+# BaseScraper pytest
+# -------------------------
 
-test_path = "lpmd/tests/utils/data/"
+test_data_id = "shipment"
+test_partition_id = "00.All"
+test_url = "https://www.e-stat.go.jp/stat-search/file-download?statInfId=000032117924&fileKind=0"
+test_error_url = "http://xxxxxx/dummy/test/xxxxxx.jp"
 
 
-class TestScrape:
-    """pytest for lpmd.core.unstable.scrape."""
+class TestBaseScraper:
+    """pytest for lpmd.core.scrape.BaseScraper"""
 
-    def test_get_shipment_df(self):
-        """Unit test for get_shipment_df."""
-        effective_url = test_url["effective"]
-        df = scp.get_shipment_df(effective_url)
-        assert isinstance(df, pd.DataFrame)
+    @pytest.fixture()
+    def setup(self):
+        self.base = BaseScraper(data_id=test_data_id)
 
-        ineffective_url = test_url["ineffective"]
-        df = scp.get_shipment_df(ineffective_url)
+    def test_init_property(self, setup):
+        assert self.base.data_id == test_data_id
+
+        with open("data_catalogue.yml", "r") as yml:
+            data_catalogue = yaml.safe_load(yml)
+        assert self.base.data_catalogue == data_catalogue[test_data_id]
+        assert self.base.columns == data_catalogue[test_data_id]["columns"]
+
+        assert self.base.datasets_path == "lpmd/datasets/{data_id}/".format(
+            data_id=test_data_id
+        )
+
+    def test_get_scraped_data(self, setup):
+        df = self.base.get_scraped_data(partition_id=test_partition_id)
+        assert len(df) > 0
+
+        self.base.data_catalogue["partition"][test_partition_id] = test_error_url
+        df = self.base.get_scraped_data(partition_id=test_partition_id)
         assert df is None
 
-    def test_save_batch_shipment_df(self):
-        """Unit test for save_batch_shipment_df."""
-        url_dict = scp.url_dict["01.shipment"]
-        exp_dict_result = dict()
-        for value in url_dict.values():
-            exp_dict_result[value] = True
+        self.base.data_catalogue["partition"][test_partition_id] = test_url
 
-        tgt_dict_result = scp.save_batch_shipment_df(test_path)
+    def test__cleanse_scraped_data(self, setup):
+        df_scraped = pd.DataFrame(
+            {
+                "year": [
+                    "平.13(2001)",
+                    "平.14(2002)",
+                    "平.15(2003)",
+                    "平.16(2004)",
+                    "平.17(2005）",
+                    "平.18(2006）",
+                ],
+                "cattle": ["…", "-", "x", "35660", "35080", "33755"],
+            }
+        )
+
+        df_scraped_exp = pd.DataFrame(
+            {
+                "year": [2001, 2002, 2003, 2004, 2005, 2006],
+                "cattle": [np.nan, np.nan, np.nan, 35660.0, 35080.0, 33755.0],
+            }
+        )
+        df_scraped_exp["year"] = df_scraped_exp["year"].astype(int)
+        df_scraped_exp["cattle"] = df_scraped_exp["cattle"].astype(float)
+
+        df_scraped_tgt = self.base._cleanse_scraped_data(df_scraped=df_scraped)
+
+        pd.testing.assert_frame_equal(df_scraped_tgt, df_scraped_exp)
+
+
+# -------------------------
+# ScraperShipment pytest
+# -------------------------
+
+test_shipment_data_id = "shipment"
+test_shipment_partition_id = "00.All"
+test_shipment_url = "https://www.e-stat.go.jp/stat-search/file-download?statInfId=000032117924&fileKind=0"
+test_shipment_error_url = "http://xxxxxx/dummy/test/xxxxxx.jp"
+
+
+class TestScraperShipment:
+    @pytest.fixture()
+    def setup(self):
+        self.scraper = ScraperShipment()
+
+    def test_get_scraped_data(self, setup):
+        df = self.scraper.get_scraped_data(partition_id=test_shipment_partition_id)
+        assert len(df) > 0
+
+        self.scraper.data_catalogue["partition"][
+            test_shipment_partition_id
+        ] = test_shipment_error_url
+        df = self.scraper.get_scraped_data(partition_id=test_shipment_partition_id)
+        assert df is None
+
+        self.scraper.data_catalogue["partition"][
+            test_shipment_partition_id
+        ] = test_shipment_url
+
+    def test_save_scraped_data(self, setup):
+        # path を指定しない場合, save 場所に何もないことを確認.
+        path = None
+        file = test_shipment_data_id + "-" + test_shipment_partition_id + ".csv"
+        path_file = os.path.join(".", test_shipment_data_id, file)
+        assert not os.path.exists(path_file)
+
+        # save されたことを確認する.
+        has_saved = self.scraper.save_scraped_data(
+            partition_id=test_shipment_partition_id, path=path
+        )
+        assert has_saved
+        assert os.path.exists(path_file)
+
+        # cleanup
+        shutil.rmtree(test_shipment_data_id)
+
+        # path を指定した場合, save 場所に何もないことを確認.
+        path = "lpmd/tests/core/.tmp"
+        path_file = os.path.join(path, test_shipment_data_id, file)
+        assert not os.path.exists(path_file)
+
+        # save されたことを確認する.
+        has_saved = self.scraper.save_scraped_data(
+            partition_id=test_shipment_partition_id, path=path
+        )
+        assert has_saved
+        assert os.path.exists(path_file)
+
+        # cleanup
+        shutil.rmtree(path)
+
+    def test_raise_save_scraped_data(self, setup):
+        msg = "Specified path must be str."
+        path = 1234567890
+        with pytest.raises(TypeError, match=msg):
+            self.scraper.save_scraped_data(
+                partition_id=test_shipment_partition_id, path=path
+            )
+
+    def test_save_batch(self, setup):
+        exp_dict_result = dict()
+        for partition_id in self.scraper.data_catalogue["partition"].keys():
+            exp_dict_result[partition_id] = True
+
+        tgt_dict_result = self.scraper.save_batch()
         assert tgt_dict_result == exp_dict_result
 
-        # Clean up
-        shutil.rmtree(test_path)
+        # cleanup
+        shutil.rmtree(self.scraper.data_id)
 
-    def test_raise_save_batch_shipment_df(self):
-        """Raise test for save_batch_shipment_df."""
-        error_path = 123456789
-        msg = "Specified path must be str."
-        with pytest.raises(TypeError, match=msg):
-            scp.save_batch_shipment_df(error_path)
+    def test_aggregate(self, setup):
+        df = self.scraper.aggregate()
+        assert len(df) > 0
+
+    def test_out_to_datasets(self, setup):
+        original_datasets_path = self.scraper.datasets_path
+        self.scraper.datasets_path = "lpmd/tests/core/.tmp/{data_id}/".format(
+            data_id=self.scraper.data_id
+        )
+        file = "{data_id}.parquet.zstd".format(data_id=self.scraper.data_id)
+
+        # 保存先パス文字列のテスト.
+        exp_file_path = os.path.join(self.scraper.datasets_path, file)
+        tgt_file_path = self.scraper.out_to_datasets()
+        assert tgt_file_path == exp_file_path
+
+        # 実際にファイルが保存されているか確認.
+        assert os.path.exists(tgt_file_path)
+
+        # cleanup
+        shutil.rmtree("lpmd/tests/core/.tmp/")
+        self.scraper.datasets_path = original_datasets_path
